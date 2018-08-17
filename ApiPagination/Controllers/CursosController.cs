62940411dc0046c87ae987912d7ec41f73f96158
tvasconcelos90 <tiagobiker1@gmail.com>
2018-08-17 15:10:55 -0300

    creating controller

---
+++ ApiPagination/Controllers/CursosController.cs
@@ -0,0 +1,17 @@
+﻿using ApiPagination.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiPagination.Controllers
+{
+    public class CursosController : ApiController
+    {
+        private ApiPaginationContext db = new ApiPaginationContext();
+
+
+    }
+}
