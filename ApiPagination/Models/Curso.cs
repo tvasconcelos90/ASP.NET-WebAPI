5a116ac7cbf91c55dd24f39f86de5d79eacfdb8d
tvasconcelos90 <tiagobiker1@gmail.com>
2018-08-17 11:49:14 -0300

    adding validations in the class model 'Cursos'

--- ApiPagination/Models/Curso.cs
+++ ApiPagination/Models/Curso.cs
@@ -1,22 +1,36 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace ApiPagination.Models
 {
+    [Table("Cursos")]
     public class Curso
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage="O título do curso deve ser preenchido.")]
+        [MaxLength(100, ErrorMessage ="O título do curso só pode conter até 100 caracteres.")]
         public string Titulo { get; set; }
 
+        [Required(ErrorMessage ="A URL do curso deve ser preenchida.")]
+        [Url(ErrorMessage ="A URL do curso deve conter um endereço válido.")]
         public string URL { get; set; }
 
+        [Required(ErrorMessage ="O canal do curso deve ser preenchido.")]
+        [JsonConverter(typeof(StringEnumConverter))]
         public Canal Canal { get; set; }
 
+        [Required(ErrorMessage ="A data de publicacao do curso deve ser preenchida.")]
         public DateTime DataPublicacao { get; set; }
 
+        [Required(ErrorMessage ="A carga horaria do curso deve ser preenchida.")]
+        [Range(1, Int32.MaxValue, ErrorMessage="A carga horaria deve ser de pelo menos 1h .")]
         public int CargaHoraria { get; set; }
     }
 }
