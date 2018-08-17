a907bdcb686eb8a7965b339f78f2b794ebb1bbf8
tvasconcelos90 <tiagobiker1@gmail.com>
2018-08-17 14:59:52 -0300

    creating dbcontext class

---
+++ ApiPagination/Models/ApiPaginationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ApiPagination.Models
+{
+    public class ApiPaginationContext : DbContext 
+    {
+        public ApiPaginationContext() : base("ApiPaginationLocal")
+        {
+
+        }
+
+        public DbSet<Curso> Cursos { get; set; }
+    }
+}
