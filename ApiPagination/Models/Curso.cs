e18fe0e08d7e559752e8491d59b78739075a1c65
tvasconcelos90 <tiagobiker1@gmail.com>
2018-08-17 11:32:18 -0300

    creating model classes

---
+++ ApiPagination/Models/Curso.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiPagination.Models
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string URL { get; set; }
+
+        public Canal Canal { get; set; }
+
+        public DateTime DataPublicacao { get; set; }
+
+        public int CargaHoraria { get; set; }
+    }
+}
