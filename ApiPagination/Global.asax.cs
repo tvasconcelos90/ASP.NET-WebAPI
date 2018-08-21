88b1876ee52c3e625e7dd23da2a8a9fb2bf56064
tvasconcelos90 <tiagobiker1@gmail.com>
2018-08-20 23:51:06 -0300

    converting date format

--- ApiPagination/Global.asax.cs
+++ ApiPagination/Global.asax.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Newtonsoft.Json;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,14 @@ namespace ApiPagination
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings =
+            new JsonSerializerSettings
+            {
+                Culture = CultureInfo.GetCultureInfo("pt-br"),
+                DateFormatString = "dd/MM/yyyy"
+            };
+
         }
     }
 }
